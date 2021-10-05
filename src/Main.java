1b8487beb18a3b1cf2cca0ffad45f65f59b7496f
Gina Sprint <sprint@gonzaga.edu>
2021-10-05 10:28:33 -0700

    finish exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -48,5 +48,64 @@ public class Main {
         // we will return to this project to talk about exceptions
         // to be continued...
 
+        // EXCEPTIONS
+        // an exception is an event at runtime that disrupts the normal program execution flow
+        // 2 types of exceptions in java
+        // unchecked exception: runtime exception and error
+        // checked exceptions: exceptions that you must anticipate, handle, and/or recover from
+
+        // example of a runtime exception
+        try {
+            int x = 5 / 0; // surround this code with a try/catch block
+        } catch (ArithmeticException e) {
+            // if something goes wrong in the try
+            // this catch will execute
+            // won't crash our program
+            // we should recover here
+            e.printStackTrace(); // System.err
+        }
+        // cmd + opt + T (mac)
+        // ctrl + alt + T (windows)
+
+        // checked exception
+        // (must acknowledge it)
+        // try to open a file that does not exist (DNE)
+        Scanner infile = null;
+
+        try {
+            infile = new Scanner(new File("dne.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // this body executes no matter what happens in the try (success or failure)
+            // one use case... clean up system resources (e.g. close any files, sockets, ...)
+            System.out.println("hello from finally");
+        }
+
+        /*
+        Practice problem: open ListExceptionFun and run the following code:
+
+        Task 1: Run the program. What happens if you enter “hi” as user input instead of an integer?
+        Is this a checked or unchecked exception? How do you know?
+        Task 2: Using a try/catch block, ensure this exception doesn’t crash your program!
+        Task 3: Write an input validation loop that requires the user enters an integer.
+         */
+        Scanner kb = new Scanner(System.in);
+        int userNum = 0;
+        boolean valid = false; // assume invalid input until proven otherwise
+        do {
+            try {
+                System.out.print("Enter an integer (or else!): ");
+                userNum = kb.nextInt();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("That's not an integer!!");
+            } finally {
+                kb.nextLine(); // clear buffer
+                // on failure: the input is still in there -> infinite loop
+                // on success: the newline is still in there -> potential problems later
+            }
+        } while (!valid);
+        System.out.println("Program execution continues...");
     }
 }
